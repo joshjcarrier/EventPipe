72262accdea563597842690e997fbd6812790934
Josh Carrier <joshjcarrier@gmail.com>
2012-06-14 00:23:54 -0700

    Robust performant RSS plugin
    - better handle retry/error conditions loading feed
    - build cache per feed and not all at once
    - support maximum items per feed

--- src/EventPipe-Server-RssFeed/RssFeedService.cs
+++ src/EventPipe-Server-RssFeed/RssFeedService.cs
@@ -12,19 +12,28 @@
 
     internal class RssFeedService
     {
-        private readonly IEnumerable<string> feeds;
+        private readonly IEnumerable<SyndicationCache> feeds;
         private readonly int refreshInterval;
         private readonly int nextInterval;
+        private readonly int maximumItems;
         private readonly RawPublishEvent publishEvent;
         private readonly TraceEvent traceEvent;
 
-        public RssFeedService(IEnumerable<string> feeds, int refreshInterval, int nextInterval, RawPublishEvent publishEvent, TraceEvent traceEvent)
+        public RssFeedService(IEnumerable<string> feeds, int refreshInterval, int nextInterval, int maximumItems, RawPublishEvent publishEvent, TraceEvent traceEvent)
         {
-            this.feeds = feeds;
             this.refreshInterval = refreshInterval;
             this.nextInterval = nextInterval;
+            this.maximumItems = maximumItems;
             this.publishEvent = publishEvent;
             this.traceEvent = traceEvent;
+
+            this.feeds = feeds.Select(p =>
+            {
+                this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Subscribing to feed: " + p });
+                return new SyndicationCache(p);
+            }).ToList();
+
+            this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = string.Format("Settings: Refresh: {0}m, Next interval: {1}ms, Max items: {2}", refreshInterval, nextInterval, maximumItems) });
 
             this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Ready" });
         }
@@ -34,7 +43,8 @@
             var feeds = configurationService.Where(p => p.Key.StartsWith("feed", StringComparison.OrdinalIgnoreCase)).Select(p => p.Value);
             var refreshInterval = int.Parse(configurationService["RefreshInterval"]);
             var nextInterval = int.Parse(configurationService["NextInterval"]);
-            return new RssFeedService(feeds, refreshInterval, nextInterval, eventAggregator.GetEvent<RawPublishEvent>(), eventAggregator.GetEvent<TraceEvent>());
+            var maxItems = int.Parse(configurationService["MaximumItems"]);
+            return new RssFeedService(feeds, refreshInterval, nextInterval, maxItems, eventAggregator.GetEvent<RawPublishEvent>(), eventAggregator.GetEvent<TraceEvent>());
         }
 
         public void Start()
@@ -47,39 +57,72 @@
         {
             while (true)
             {
-                var cache = new List<string>();
                 foreach (var feed in this.feeds)
                 {
-                    var reader = XmlReader.Create(feed);
-                    var feedReader = SyndicationFeed.Load(reader);
-
-                    foreach (var item in feedReader.Items)
+                    // cache needs to be refreshed
+                    if (DateTime.Now.Subtract(feed.LastRefreshed).TotalMinutes > this.refreshInterval)
                     {
-                        var payload = (char)PacketDataType.Text + " " + string.Format("{0,-20}{1}", feedReader.Title.Text.Substring(0, Math.Min(20, feedReader.Title.Text.Length)), item.Title.Text);
-                        cache.Add(payload);
-                    }
-
-                    this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Cached " + feed });
-                }
-
-                var lastRefreshed = DateTime.Now;
-                while (DateTime.Now.Subtract(lastRefreshed).TotalMinutes < this.refreshInterval)
-                {
-                    foreach (var cacheItem in cache)
-                    {
-                        this.publishEvent.Publish(cacheItem);
-
-                        // refetch feeds if it's time to
-                        if (DateTime.Now.Subtract(lastRefreshed).TotalMinutes > this.refreshInterval)
+                        // try up to 3 times
+                        for (var retries = 0; retries < 3; retries++)
                         {
+                            var cache = new List<string>();
+
+                            try
+                            {
+                                this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Loading feed: " + feed.Source });
+
+                                var reader = XmlReader.Create(feed.Source);
+                                var feedReader = SyndicationFeed.Load(reader);
+
+                                foreach (var item in feedReader.Items.Take(this.maximumItems))
+                                {
+                                    var payload = (char)PacketDataType.Text + " " + string.Format("{0,-20}{1}", feedReader.Title.Text.Substring(0, Math.Min(20, feedReader.Title.Text.Length)), item.Title.Text);
+                                    cache.Add(payload);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Failed to load: " + feed.Source + " " + ex.Message });
+                                continue;
+                            }
+
+                            feed.LastRefreshed = DateTime.Now;
+                            feed.PayloadCache = cache;
+                            this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Cached feed: " + feed.Source });
                             break;
                         }
+                    }
+                    else
+                    {
+                        this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Reloading cached feed: " + feed.Source });
+                    }
+
+                    foreach (var cacheItem in feed.PayloadCache)
+                    {
+                        this.traceEvent.Publish(new TraceMessage { Owner = "RssFeed", Message = "Broadcast item: " + feed.Source + " - " + cacheItem });
+                        this.publishEvent.Publish(cacheItem);
 
                         // sleep i said
                         Thread.Sleep(this.nextInterval);
                     }
                 }
             }
+        }
+
+        private class SyndicationCache
+        {
+            public SyndicationCache(string feed)
+            {
+                this.Source = feed;
+                this.LastRefreshed = DateTime.MinValue;
+                this.PayloadCache = new List<string>();
+            }
+
+            public string Source { get; private set; }
+
+            public DateTime LastRefreshed { get; set; }
+
+            public List<string> PayloadCache { get; set; }
         }
     }
 }
